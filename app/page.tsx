c8e982cce36254c719de713797c24a24b0027634
Angeles Maza <angie.maza.89@gamil.com>
2024-07-27 02:44:56 -0300

    nuevo favico y metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center">
         <div
-          className="bg-cover bg-center w-full h-full flex justify-center items-center min-h-screen min-w-screen"
+          className="bg-cover bg-slate-400 bg-center w-full h-full flex justify-center items-center min-h-screen min-w-screen"
           style={{ backgroundImage: "url('porx-bg.png')" }}
         >
           <h1 className="text-center font-bold text-[50px] lg:text-[100px]">Próximamente</h1>
