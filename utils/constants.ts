83b8bd60b8515041a0f1208ebd22f921bc4cb540
Angeles Maza <angie.maza.89@gamil.com>
2024-08-18 20:13:56 -0300

    adding worflows

---
+++ utils/constants.ts
@@ -0,0 +1,28 @@
+export const images = [
+    {
+        id: 1,
+        src: "/image1.jpg",
+        alt: "image 1"
+    },
+    {
+        id: 2,
+        src: "/image2.jpg",
+        alt: "image 2"
+    },
+    {
+        id: 4,
+        src: "/image4.jpg",
+        alt: "image 4"
+    },
+    {
+        id: 5,
+        src: "/image5.jpg",
+        alt: "image 5"
+    },
+    {
+        id: 6,
+        src: "/image6.jpg",
+        alt: "image 6"
+    }
+   
+]
