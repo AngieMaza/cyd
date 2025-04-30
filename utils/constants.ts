6621febbb18cdbcf2c1d776548efe8e98bf72676
Angeles Maza <angie.maza.89@gamil.com>
2025-04-30 13:41:19 -0300

    update photos

--- utils/constants.ts
+++ utils/constants.ts
@@ -10,6 +10,11 @@ export const images = [
         alt: "image 2"
     },
     {
+        id: 3,
+        src: "/image3.jpg",
+        alt: "image 3"
+    },
+    {
         id: 4,
         src: "/image4.jpg",
         alt: "image 4"
@@ -23,6 +28,55 @@ export const images = [
         id: 6,
         src: "/image6.jpg",
         alt: "image 6"
-    }
-   
+    },
+    {
+        id: 7,
+        src: "/image7.jpg",
+        alt: "image 7"
+    },
+    {
+        id: 8,
+        src: "/image8.jpg",
+        alt: "image 8"
+    },
+    {
+        id: 9,
+        src: "/image9.jpg",
+        alt: "image 9"
+    },
+    {
+        id: 10,
+        src: "/image10.jpg",
+        alt: "image 10"
+    },
+    {
+        id: 11,
+        src: "/image11.jpg",
+        alt: "image 11"
+    },
+    {
+        id: 12,
+        src: "/image12.jpg",
+        alt: "image 12"
+    },
+    {
+        id: 13,
+        src: "/image13.jpg",
+        alt: "image 13"
+    },
+    {
+        id: 14,
+        src: "/image14.jpg",
+        alt: "image 14"
+    },
+    {
+        id: 15,
+        src: "/image15.jpg",
+        alt: "image 15"
+    },
+    {
+        id: 16,
+        src: "/image16.jpg",
+        alt: "image 16",
+    },
 ]
